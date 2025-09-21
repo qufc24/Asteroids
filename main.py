e5b77d479bf443327bf2e395e631e65d91edf888
Qufc <adam.szymanski41@proton.me>
2025-09-21 15:47:45 +0200

    Final version for Boot

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from constants import *
+from asteroid import *
 from player import *
 import pygame
-
+from asteroidfield import *
+import sys
+from shot import Shot
 
 def main():
 
@@ -10,10 +13,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}\n\n")
 
-    #Initializing
+    #Initializing aka Clusterfuck
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updateable, drawable)
+    Shot.containers = (shots, updateable, drawable)
+    AsteroidField.containers = updateable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0 #frames
@@ -23,9 +38,22 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updateable.update(dt)
+
+        for obj in asteroids:
+            if obj.collison(player):
+                print("Game over!")
+                sys.exit()
+            for bullet in shots:
+                if obj.collison(bullet):
+                    obj.split()
+                    bullet.kill()
+
         screen.fill((0,0,0))
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = (clock.tick(60)/1000)
